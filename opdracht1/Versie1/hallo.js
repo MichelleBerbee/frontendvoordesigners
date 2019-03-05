d2bab7c19e628462fc5f01ae639b0d5408ca3ed9
Michelle <berbeem@hva.nl>
2019-03-05 11:03:01 +0100

    nieuw

--- opdracht1/Versie1/hallo.js
+++ opdracht1/Versie1/hallo.js
@@ -1,9 +1,11 @@
-var button = document.querySelector("footer button")
-
-button.addEventListener("click",function(e){
-  e.preventDefault()
+var buttons = document.querySelectorAll("footer button")
 
 
-  var test = document.querySelector(".hallo")
-  test.classList.toggle("active")
-})
+for(let i = 0; i < buttons.length; i++){ //door de rij heen
+  buttons[i].addEventListener("click",function(e){
+    e.preventDefault() //niet opnieuw laden
+
+  var section = e.target.parentNode.nextElementSibling
+    section.classList.toggle("active")
+  })
+}
